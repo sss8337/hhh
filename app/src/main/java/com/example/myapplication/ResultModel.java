483b5a15901bf39e30e092dcb02a6556c31f639c
sss8337 <wkrcjs8337@naver.com>
2023-05-19 19:57:05 +0900

    오류 2개

---
+++ app/src/main/java/com/example/myapplication/ResultModel.java
@@ -0,0 +1,22 @@
+package com.example.myapplication;
+
+//import com.codingstuff.quizzyapp.views.ResultFragment;
+
+public class ResultModel {
+
+    private int correct ,wrong, notAnswered;
+
+    public ResultModel(){}
+
+    public int getWrong() {
+        return wrong;
+    }
+
+    public int getCorrect() {
+        return correct;
+    }
+
+    public int getNotAnswered() {
+        return notAnswered;
+    }
+}
